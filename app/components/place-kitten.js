77beeb06c294ef9aac0a5b7b54332763b52fb47f
codepreneur <contramundee@gmail.com>
2014-11-22 04:30:03 +0000

    changed length to height and added /g/

--- app/components/place-kitten.js
+++ app/components/place-kitten.js
@@ -1,11 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	length: 100, // default length
+	height: 100, // default height
 	width: 100, // default width
 
-	src: Ember.computed('length', 'width', function(){
-		var base = 'http://placekitten.com/';
-		return base + this.get('width') + '/' + this.get('length');
+	src: Ember.computed('height', 'width', function(){
+		var base = 'http://placekitten.com/g/';
+		return base + this.get('width') + '/' + this.get('height');
 	})
 });
